60566d7318524ad051df2b6d723d9d344a96fed6
tirthankarlex <tirthankar.dey@lexplosion.in>
2018-11-19 19:14:37 +0530

    reports api's added

--- controllers/komrisk.controller.js
+++ controllers/komrisk.controller.js
@@ -42,3 +42,185 @@ const login = async function(req, res) {
 	}
 }
 module.exports.login = login;
+
+const compStatus = async function(req, res) {
+	var dataMap = {
+		title: "Compliance Status",
+		subTitle: req.body.start + " - " + req.body.end,
+		xAxisName: "Compliance",
+		yAxisName: "Compliance(Count)",
+		chartData: [{
+			label: "NON COMPLIANT",
+			color: "FF0000",
+			value: 20,
+			link: {
+				dataFilter: "",
+				type: "NON COMPLIANT",
+				userFilter: ""
+			}
+		}, {
+			label: "COMPLIANT",
+			color: "00FF00",
+			value: 7,
+			link: {
+				dataFilter: "",
+				type: "COMPLIANT",
+				userFilter: ""
+			}
+		}, {
+			label: "LATE COMPLIANT",
+			color: "FFBF00",
+			value: 17,
+			link: {
+				dataFilter: "",
+				type: "LATE COMPLIANT",
+				userFilter: ""
+			}
+		}]
+	};
+
+	return res.json(dataMap);
+}
+module.exports.compStatus = compStatus;
+
+const compActStatus = async function(req, res) {
+	var dataMap = {
+		title: "Compliance Activity Status",
+		subTitle: req.body.start + " - " + req.body.end,
+		xAxisName: "Compliance",
+		yAxisName: "Compliance(Count)",
+		chartData: [{
+			label: "PENDING",
+			color: "FF0000",
+			value: 20,
+			link: {
+				dataFilter: "",
+				type: "pending",
+				userFilter: ""
+			}
+		}, {
+			label: "COMPLETED",
+			color: "00FF00",
+			value: 27,
+			link: {
+				dataFilter: "",
+				type: "completed",
+				userFilter: ""
+			}
+		}, {
+			label: "INITIATED",
+			color: "FFBF00",
+			value: 17,
+			link: {
+				dataFilter: "",
+				type: "initiated",
+				userFilter: ""
+			}
+		}]
+	};
+
+	return res.json(dataMap);
+}
+module.exports.compActStatus = compActStatus;
+
+const impactAnalysis = async function(req, res) {
+	var dataMap = {
+		title: "Impact Analysis of Compliances Pending Completion",
+		subTitle: req.body.start + " - " + req.body.end,
+		xAxisName: "Compliance",
+		yAxisName: "Compliance(Count)",
+		chartData: [{
+			label: "HIGH",
+			color: "FF0000",
+			value: 20,
+			link: {
+				dataFilter: "",
+				type: "High",
+				userFilter: ""
+			}
+		}, {
+			label: "LOW",
+			color: "00FF00",
+			value: 27,
+			link: {
+				dataFilter: "",
+				type: "Low",
+				userFilter: ""
+			}
+		}, {
+			label: "MEDIUM",
+			color: "FFBF00",
+			value: 17,
+			link: {
+				dataFilter: "",
+				type: "Medium",
+				userFilter: ""
+			}
+		}]
+	};
+
+	return res.json(dataMap);
+}
+module.exports.impactAnalysis = impactAnalysis;
+
+const incActStatus = async function(req, res) {
+	var dataMap = {
+		title: "Incident Activity Status",
+		subTitle: req.body.start + " - " + req.body.end,
+		xAxisName: "Incident",
+		yAxisName: "Incident(Count)",
+		chartData: [{
+			label: "COMPLETED",
+			color: "00FF00",
+			value: 27,
+			status: "completed",
+			userFilter: ""
+		}, {
+			label: "INITIATED",
+			color: "FFBF00",
+			value: 20,
+			status: "initiated",
+			userFilter: ""
+		}, {
+			label: "PENDING",
+			color: "FF0000",
+			value: 10,
+			status: "pending",
+			userFilter: ""
+		}, {
+			label: "REJECTED",
+			color: "FF0000",
+			value: 12,
+			status: "rejected",
+			userFilter: ""
+		}]
+	};
+
+	return res.json(dataMap);
+}
+module.exports.incActStatus = incActStatus;
+
+const incStatus = async function(req, res) {
+	var dataMap = {
+		title: "Incident Comparison",
+		subTitle: req.body.start + " - " + req.body.end,
+		xAxisName: "Incident",
+		yAxisName: "Incident(Count)",
+		chartData: [{
+			label: "NON-COMPLETION",
+			color: "FF0000",
+			value: 27,
+			comparison: "NON-COMPLETION",
+			userFilter: ""
+		}, {
+			label: "LATE-COMPLETION",
+			color: "FFBF00",
+			value: 10,
+			status: "LATE-COMPLETION",
+			userFilter: ""
+		}]
+	};
+
+	return res.json(dataMap);
+}
+module.exports.incStatus = incStatus;
--- routes/komrisk.js
+++ routes/komrisk.js
@@ -11,4 +11,10 @@ const komriskAuth = require('./../middleware/komrisk-auth');
 router.post('/auth/authURL', komriskAuth.authenticateApiKey, KomriskController.authURL);
 router.post('/auth/login', komriskAuth.authenticateApiKey, KomriskController.login);
 
+router.post('/complianceReports/comparisonData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.compStatus);
+router.post('/complianceReports/activityStatusData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.compActStatus);
+router.post('/complianceReports/impactAnalysisData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.impactAnalysis);
+router.post('/incidentReports/activityStatusData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.incActStatus);
+router.post('/incidentReports/comparisonData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.incStatus);
+
 module.exports = router;
