3cbbe18590939b5d92dda82253ea932be71612ab
tirthankarlex <tirthankar.dey@lexplosion.in>
2018-11-20 16:53:12 +0530

    User Access Details api added

--- routes/komrisk.js
+++ routes/komrisk.js
@@ -11,7 +11,7 @@ const komriskAuth = require('./../middleware/komrisk-auth');
 router.post('/auth/authURL', komriskAuth.authenticateApiKey, KomriskController.authURL);
 router.post('/auth/login', komriskAuth.authenticateApiKey, KomriskController.login);
 
-router.post('/users/accessDetails', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.userAccessDetails);
+router.get('/users/accessDetails', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.userAccessDetails);
 router.post('/complianceReports/comparisonData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.compStatus);
 router.post('/complianceReports/activityStatusData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.compActStatus);
 router.post('/complianceReports/impactAnalysisData', komriskAuth.authenticateApiKey, komriskAuth.authenticateToken, KomriskController.impactAnalysis);
